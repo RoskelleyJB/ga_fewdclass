342c9a6ef7de6fff2fbd6ee7c2d32ac2b63a1174
Jake <Jacob@Jacobs-MacBook-Pro.local>
2015-08-12 21:01:33 -0400

    Add startup matchmaker project

--- week_2/04_layout/food_fight/js/script.js
+++ week_2/04_layout/food_fight/js/script.js
@@ -1,8 +1,26 @@
 $(document).ready(function(){
   $(".hero h1").fadeIn(4000);
-  $(".hero h1").removeClass("hidden");
   setTimeout(function() {
     $(".hero div").fadeIn(4000);
-    $(".hero div").removeClass("hidden");
   }, 3000);
+  // Hide the extra content initially, using JS so that if JS is disabled, no problemo:
+  $('.read-more-content').addClass('hidden')
+
+  // Set up a link to expand the hidden content:
+  .before('<a class="read-more-show read-more" href="#"><p>Read More ></p></a>')
+    
+  // Set up a link to hide the expanded content.
+  .append(' <a class="read-more-hide read-more" href="#"><p>Show Less</p></a>');
+
+  // Set up the toggle effect:
+  $('.read-more-show').on('click', function(e) {
+    $(this).next('.read-more-content').removeClass('hidden');
+    $(this).addClass('hidden');
+    e.preventDefault();
+  });
+
+  $('.read-more-hide').on('click', function(e) {
+    $(this).parent('.read-more-content').addClass('hidden').parent().children('.read-more-show').removeClass('hidden');
+    e.preventDefault();
+  });
 });
